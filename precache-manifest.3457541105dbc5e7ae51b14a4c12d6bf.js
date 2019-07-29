36cc7d66837df0a6ed0894582d25578c17078bf9
ronyshchenko <ronyshchenko@gmail.com>
2019-07-29 22:34:06 +0300

    Updates

---
+++ precache-manifest.3457541105dbc5e7ae51b14a4c12d6bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9755691ba5e007b2b21a8b42d31975",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "1a67691e83c6d04d6098",
+    "url": "/react_static-list-of-todos/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-todos/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "1a67691e83c6d04d6098",
+    "url": "/react_static-list-of-todos/static/js/main.21830b3f.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
